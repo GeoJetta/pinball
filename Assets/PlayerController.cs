a73477dcad65dcd002340f49d02028521dc29191
GeoJetta <geoffrey.smith@ccnth.org>
2017-09-28 15:01:12 -0700

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveVertical, 0.0f, 0.0f);
+
+		rb.AddForce (movement * 5000);
+	}
+}
